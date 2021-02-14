d37062806a42c07ae002d99a887c8c6baac4c95d
Pedro Luis Brillhadori <pedroluisbrilhadori@gmail.com>
2021-02-14 10:28:56 -0300

    já esta funcionando

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-var links = ['https://heeeeeeeey.com/', '']
+var links = ['https://heeeeeeeey.com/', 'https://pointerpointer.com/', 'http://randomcolour.com', 'https://cat-bounce.com/', 
+'http://www.movenowthinklater.com/', 'http://endless.horse/', 'https://smashthewalls.com/', 'https://theuselessweb.com/', 
+]
 
 
 function Redirect() {
-    window.location.href = 'https://heeeeeeeey.com/'
+    let linkAleatorio 
+    function randon() {
+        linkAleatorio = links[Math.floor(Math.random() * links.length )]
+    }
+    randon();;
+    console.log(linkAleatorio)
+    window.location.href = linkAleatorio
 }
